19a6bb1bfa0698d14950c92e3e7d6f06d447979b
Qxf2 <mak@qxf2.com>
2019-06-12 18:33:44 +0530

    Copied files from our framework

---
+++ conf/locators_conf.py
@@ -0,0 +1,19 @@
+#Common locator file for all locators
+#Locators are ordered alphabetically
+
+############################################
+#Selectors we can use
+#ID
+#NAME
+#css selector
+#CLASS_NAME
+#LINK_TEXT
+#PARTIAL_LINK_TEXT
+#XPATH
+###########################################
+
+#Locators for the footer object(footer_object.py)
+USERNAME_FIELD = "id,id_username"
+PASSWORD_FIELD = "name,password"
+LOGIN_BUTTON = "xpath,//button[text()='Login']"
+LOGOUT_LINK = "xpath,//a[text()='Logout']"
