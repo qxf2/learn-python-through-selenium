4b62059f8b82050d40823544cdcf206d49a7b4d5
Qxf2 <mak@qxf2.com>
2019-06-27 18:43:18 +0530

    removed a return statement

--- page_objects/Product_Object.py
+++ page_objects/Product_Object.py
@@ -120,8 +120,6 @@ class Product_Object():
         positive="The cart currently has %d items"%self.CART_QUANTITY,
         negative="")
 
-        return cart_quantity
-
     def add_product(self,product_name):
         "Add the lowest priced product with the filter condition in name"
         before_cart_quantity = self.get_current_cart_quantity() 
